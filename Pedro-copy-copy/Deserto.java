f9f58c8653358631932e66a329ff8986b58be1be
Pedro Henrique Moraes Batista <pedrorubin04@gmail.com>
2025-09-09 14:03:47 -0300

    Add files via upload

---
+++ Pedro-copy-copy/Deserto.java
@@ -0,0 +1,19 @@
+// WARNING: This file is auto-generated and any changes to it will be overwritten
+import lang.stride.*;
+import java.util.*;
+import greenfoot.*;
+
+/**
+ * 
+ */
+public class Deserto extends World
+{
+
+    /**
+     * Constructor for objects of class Deserto.
+     */
+    public Deserto()
+    {
+        super(700, 400, 2);
+    }
+}
---
+++ Pedro-copy-copy/Sapo.java
@@ -0,0 +1,28 @@
+// WARNING: This file is auto-generated and any changes to it will be overwritten
+import lang.stride.*;
+import java.util.*;
+import greenfoot.*;
+
+/**
+ * 
+ */
+public class Sapo extends Actor
+{
+
+    /**
+     * Act - do whatever the Sapo wants to do. This method is called whenever the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        move(6);
+        if (Greenfoot.isKeyDown("left")) {
+            turn(-5);
+        }
+        if (Greenfoot.isKeyDown("right")) {
+            turn(5);
+        }
+        if (Greenfoot.isKeyDown("up")) {
+            turn(8);
+        }
+    }
+}
